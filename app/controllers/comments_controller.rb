15cd5c4379feaf54441161a0598151d5eae2d5b7
akhiln6 <akhiln6@gmail.com>
2023-08-08 16:11:30 +0530

    create action is written in comments controller and comment is created in database

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,5 +5,21 @@ class CommentsController < ApplicationController
 	def new
 		
 	end
+	def create 
+		@tweet = Tweet.find(params[:tweet_id])
+
+    @comment = @tweet.comments.create(comment_params)
+    redirect_to root_path
+
+		
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:body).merge(user_id: current_user.id)
+	end
+
+
+
 
 end
