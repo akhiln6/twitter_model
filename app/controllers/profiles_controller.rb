9ba5020aca73068dd5654a102aef9876a69678e7
akhiln6 <akhiln6@gmail.com>
2023-08-25 12:50:03 +0530

    form for mobile number and address created and it inserted into database and showed in show page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,7 +6,27 @@ class ProfilesController < ApplicationController
 
   def show
     
-    
+    @profile = Profile.new
+    @profiles = Profile.all
+  end
+  
+  def new
+    @profile = Profile.new
+  end
+
+  def create 
+    @profile = current_user.profile.create(profile_params)
+    redirect_to root_path
+  end
+
+
+
+
+ 
+  private
+
+  def profile_params
+    params.require(:profile).permit(:mobilenumber, :address)
   end
 
 end
